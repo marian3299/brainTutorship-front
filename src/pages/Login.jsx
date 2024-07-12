06b23d9d10b7d27fefef07a702f4b108facf7b8b
mariana3299 <mariana.ag3299@gmail.com>
2024-07-11 20:29:47 -0600

    feat: form functionality to Register component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,22 +4,30 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <>
-    <div className="bg-blue-1 h-[100dvh] flex flex-col justify-center items-center gap-3">
-      <h1 className="text-white text-4xl mb-12 font-bold">Iniciar sesión</h1>
-      <form action="">
-        <div className="flex flex-col gap-4 items-center justify-center">
-          <input type="text" placeholder="Correo" className="input  w-80" />
-          <input type="password" placeholder="Contraseña" className="input  w-80" />
-          <SubmitButton>Iniciar sesión</SubmitButton>
-        </div>
-      </form>
+      <div className="bg-blue-1 h-[100dvh] flex flex-col justify-center items-center gap-3">
+        <h1 className="text-white text-4xl mb-12 font-bold">Iniciar sesión</h1>
+        <form action="">
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <input
+              type="text"
+              placeholder="Correo"
+              className="input  w-80"
+              id="correo"
+            />
+            <input
+              type="password"
+              placeholder="Contraseña"
+              className="input  w-80"
+              id="password"
+            />
+            <SubmitButton>Iniciar sesión</SubmitButton>
+          </div>
+        </form>
 
-      <Link to="/register">
-        <a href="" className="text-white underline font-light">¿No tienes cuenta? Registrate aquí</a>
-      </Link>
-      
-    </div>
-      
+        <Link to="/register" className="text-white underline font-light">
+          ¿No tienes cuenta? Registrate aquí
+        </Link>
+      </div>
     </>
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,24 +1,135 @@
 import SubmitButton from "../utils/SubmitButton";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  console.log(errors);
+
   return (
     <div className="flex flex-col bg-blue-1 h-[100dvh] items-center justify-center gap-3">
       <h1 className="text-white text-4xl mb-12 font-bold">Crear cuenta</h1>
-      <form action="">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <input type="text" placeholder="Nombre" className="input  w-80" />
-          <input type="text" placeholder="Apellido" className="input  w-80" />
-          <input type="text" placeholder="Correo" className="input  w-80" />
-          <input type="password" placeholder="Contraseña" className="input  w-80" />
-          <input type="password" placeholder="Repetir contraseña" className="input  w-80" />
+      <div className="w-[340px]">
+        <form onSubmit={onSubmit} noValidate>
+          <div className="mb-5">
+            {errors.name && (
+              <span className="text-red-600 font-semibold text-xs mb-1">
+                *{errors.name.message}
+              </span>
+            )}
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="input  w-full text-white"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Nombre requerido",
+                },
+              })}
+            />
+          </div>
+
+          <div className="mb-5">
+            {errors.lastname && (
+              <span className="text-red-600 font-semibold text-xs mb-1">
+                *{errors.lastname.message}
+              </span>
+            )}
+            <input
+              type="text"
+              placeholder="Apellido"
+              className="input w-full text-white"
+              {...register("lastname", {
+                required: {
+                  value: true,
+                  message: "Apellido requerido",
+                },
+              })}
+            />
+          </div>
+
+          <div className="mb-5">
+            {errors.email && (
+              <span className="text-red-600 font-semibold text-xs mb-1">
+                *{errors.email.message}
+              </span>
+            )}
+            <input
+              type="text"
+              placeholder="Correo"
+              className="input  w-full text-white"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email requerido",
+                },
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Correo no valido",
+                },
+              })}
+            />
+          </div>
+
+          <div className="mb-5">
+            {errors.password && (
+              <span className="text-red-600 font-semibold text-xs mb-1">
+                *{errors.password.message}
+              </span>
+            )}
+            <input
+              type="password"
+              placeholder="Contraseña"
+              className="input  w-full text-white"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Contraseña requerida",
+                },
+                minLength: {
+                  value: 6,
+                  message: "La contraseña debe tener al menos 6 caracteres",
+                },
+              })}
+            />
+          </div>
+
+          <div className="mb-5">
+            {errors.confirmPassword && (
+              <span className="text-red-600 font-semibold text-xs mb-1">
+                *{errors.confirmPassword.message}
+              </span>
+            )}
+            <input
+              type="password"
+              placeholder="Repetir contraseña"
+              className="input  w-full text-white"
+              {...register("confirmPassword", {
+                required: {
+                  value: true,
+                  message: "Campo requerido",
+                },
+                validate: (value) => value === watch("password") ? true : "Las contraseñas no coinciden",
+              })}
+            />
+          </div>
+
           <SubmitButton>REGISTRARSE</SubmitButton>
-        </div>
-      </form>
-      <Link to="/">
-        <a href="" className="text-white underline font-light">
-          ¿Ya tienes una cuenta? Ingrese aquí
-        </a>
+        </form>
+      </div>
+      <Link to="/" className="text-white underline font-light">
+        ¿Ya tienes una cuenta? Ingrese aquí
       </Link>
     </div>
   );
